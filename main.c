b75ca2c37ef41451d7fe183b15cac58403d4519e
GhelloZ <gustavozuffellato@icloud.com>
2025-08-25 20:46:26 +0200

    Handled missing subcommand issues.  not followed by anything doesn't give a segfault anymore

--- main.c
+++ main.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <ctype.h>
+
 #include "stats_calc.h"
 
 #include "log/utils.h"
@@ -9,6 +11,7 @@
 #include "log/deck.h"
 
 void game(const char* subcommand){
+	// Checking if the subcommand for game is a valid one and calling the appropriate function
 	if(strcmp(subcommand,"add") == 0){
 		add_game();
 	}else if(strcmp(subcommand,"delete") == 0){
@@ -33,6 +36,15 @@ int main(int argc, char* argv[]){
 	}
 
 	if(strcmp(command, "game") == 0){
+		// Checking if the user provided a subcommand
+		if(argc < 3){
+			fprintf(stderr,"Please provide a subcommand\n");
+			return 1;
+		}else if(argv[2][0] == '-'){
+			fprintf(stderr,"Please provide a subcommand before the parameters\n");
+			return 1;
+		}
+
 		game(argv[2]);
 	}else if(strcmp(command, "player") == 0){
 		printf("This command also exists\n");
